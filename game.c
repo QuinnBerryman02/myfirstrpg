640ad7dfd8d5a340188d1215ab6595e822588c21
QuinnBerryman <quinn.berryman@ucdconnect.ie>
2020-09-30 07:44:45 +0100

    Finished move() function and added player position

--- game.c
+++ game.c
@@ -3,6 +3,7 @@
 int startNewGame;
 int gameNotOver = 1;
 char playerMove;
+int player[2] = {7,3};
 int map[9][7] = { // Looked up how to declare a matrix at https://beginnersbook.com/2014/01/2d-arrays-in-c-example/
   {0,0,0,0,0,0,0},
   {0,9,8,7,8,9,0},
@@ -39,6 +40,8 @@ void newGame () {
   } else {
     printf("GAME START!\n");
     gameNotOver = 1;
+    player[0] = 7;
+    player[1] = 3;
     game();
   }
 }
@@ -53,21 +56,46 @@ void game () {
 
 void cycle () {
   move();
-  gameNotOver = 0;
+  
+//  gameNotOver = 0;
 }
 
 void move () {
   playerMove = ' ';
   printf("MOVE WHERE?\t");
   scanf(" %c", &playerMove); // couldnt figure out why scanf("%c") wasnt working, apparently scanf reads the enter key as an input and so gets skipped https://stackoverflow.com/questions/29775323/scanf-function-seems-to-be-skipped-in-c#:~:text=The%20problem%20is%20because%20of,second%20call%20to%20scanf()%20.&text=So%2C%20the%20newline%20character%20is,for%20input%20for%20that%20variable.
-  switch (playerMove) { 
+  switch (playerMove) {
     case 'w':
+      if (map[player[0]-1][player[1]] == 0) {
+        printf("%c IS NOT VALID, USE WASD!\n", playerMove);
+        move();
+      } else {
+        player[0] = player[0] - 1;
+      }
       break;
     case 'a':
+    if (map[player[0]][player[1]-1] == 0) {
+      printf("%c IS NOT VALID, USE WASD!\n", playerMove);
+      move();
+    } else {
+      player[1] = player[1] - 1;
+    }
       break;
     case 's':
+    if (map[player[0]+1][player[1]] == 0) {
+      printf("%c IS NOT VALID, USE WASD!\n", playerMove);
+      move();
+    } else {
+      player[0] = player[0] + 1;
+    }
       break;
     case 'd':
+    if (map[player[0]][player[1]+1] == 0) {
+      printf("%c IS NOT VALID, USE WASD!\n", playerMove);
+      move();
+    } else {
+      player[1] = player[1] + 1;
+    }
       break;
     default:
       printf("%c IS NOT VALID, USE WASD!\n", playerMove);
