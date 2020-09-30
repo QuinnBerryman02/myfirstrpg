6a3649892414a9de11fa59858bb71eeab2c6d8c8
QuinnBerryman <quinn.berryman@ucdconnect.ie>
2020-09-30 06:39:07 +0100

    Basic layout of functions

---
+++ game.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+char move ();
+void cycle ();
+void display();
+int fight ();
+void playerTurn ();
+void enemyTurn ();
+void gameOver();
+int newGame();
+
+int main () {
+
+  return 0;
+}
